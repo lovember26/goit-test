ce50283af35fb3ae5b880f5d21058c37bb825c49
Tetiana Prokopchuk <tetyanaprokopchuk@ukr.net>
2023-05-11 11:08:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="tweets" element={<Tweets />} />
-      <Route path="*" element={<Navigate to="/" />} />
+      <Route path="*" element={<HomePage />} />
     </Routes>
   );
 }
